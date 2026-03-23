a2d83b1cf72df80a7be46702df3989b9b536c2df
PRIYATOSH RANA <ranapriyatosh@gmail.com>
2026-03-23 16:47:16 +0530

    Delete 6.py

--- 6.py
+++
@@ -1,7 +0,0 @@
-mixed_tuple = (5, "hello", 12, 3.5, 8, 20, "world")
-mixed_list = list(mixed_tuple)
-
-filtered_list = [x for x in mixed_list if not (isinstance(x, int) and x < 10)]
-
-filtered_tuple = tuple(filtered_list)
-print("Filtered tuple:", filtered_tuple)
