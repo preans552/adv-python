8582fb1b8cf54c1c22eb54daf3e197c8463e14be
PRIYATOSH RANA <ranapriyatosh@gmail.com>
2026-03-23 16:44:12 +0530

    Delete 14.py

--- 14.py
+++
@@ -1,9 +0,0 @@
-def custom_power(base, exponent):
-    result = 1
-    for _ in range(exponent):
-        result *= base
-    return result
-
-b = int(input("Enter base: "))
-e = int(input("Enter exponent: "))
-print(f"{b}^{e} = {custom_power(b, e)}")
